00a039593f31505901c4641a7d2931ad709a3709
thbd <thiagobalbino91@gmail.com>
2022-10-13 21:44:20 -0300

    init commit

---
+++ src/main/java/br/com/venda/MicroserviceVendaApplication.java
@@ -0,0 +1,13 @@
+package br.com.venda;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicroserviceVendaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MicroserviceVendaApplication.class, args);
+	}
+
+}
